a7eb00309ebcac715c8112562d49dfe833891be5
EnzitoOFC <106297033+EnzoVaselevich@users.noreply.github.com>
2024-05-18 17:44:20 -0300

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,56 +1,45 @@
 @@ -1,70 +1,56 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import cheerio from 'cheerio'; 
 import fg from 'api-dylux'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `✳️ ejemplo ${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
+import { tiktokdl } from '@bochilteam/scraper'
 
-   let res = await igdl(args[0])
-    for (let result of res.data) {
-    conn.sendFile(m.chat, result.url, 'igdl.mp4', ``, m)
-  }
-}
-handler.help = ['instagram <link ig>']
-handler.tags = ['dl']
-handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
+var handler = async (m, { conn, text, args, usedPrefix, command}) => {
+
+if (!args[0]) throw `*𝘌𝘫𝘦𝘮𝘱𝘭𝘰:\n${usedPrefix + command} https://vm.tiktok.com/ZMjbonqPu/`
+if (!args[0].match(/tiktok/gi)) throw `*⚠️ Link Correcto?*`
+
+
+const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `${waitt}`, edit: key})
+await delay(1000 * 1);
+await conn.sendMessage(m.chat, {text: `${waittt}`, edit: key})
+await delay(1000 * 1)
+await conn.sendMessage(m.chat, {text: `${waitttt}`, edit: key})
+
+try {
+let p = await fg.tiktok(args[0])
+let te = `Name: ${p.nickname}
+Usuario" ${p.unique_id}
+Duracion: ${p.duration}
+Info: ${p.description}`
+conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
+} catch {
+try {
+const { author: { nickname }, video, description } = await tiktokdl(args[0])
+const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
+if (!url) throw '*⚠️ ERROR AL DESACARGAR EL VÍDEO*'
+conn.sendFile(m.chat, url, 'fb.mp4', `• 🧃 *Nombre:* ${nickname}\n• 📄 *Descripción:* ${description}`, m)
+m.react(done)
+} catch {
+m.reply(`*⚠️ ERROR AL DESCARGAR EL VÍDEO*`)
+}}
+    handler.help = ['tiktok']
+handler.tags = ['descargas']
+handler.command = /^(tiktok|tt|ttdl|tiktokdl|tiktoknowm)$/i
+handler.register = true
 handler.diamond = true
 
-export default handler 
+export default handler
 
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-async function igdl(url) {
-   try {
-      const response = await axios.post("https://saveig.app/api/ajaxSearch", new URLSearchParams({ q: url, t: "media", lang: "en" }).toString(), {
-         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Origin': 'https://saveig.app/en',
-            'Referer': 'https://saveig.app/en',
-            'Referrer-Policy': 'strict-origin-when-cross-origin',
-            'User-Agent': 'PostmanRuntime/7.31.1'
-         }
-      });
-
-      const $ = cheerio.load(response.data.data);
-      const data = $('div.download-items__btn').map((i, e) => {
-         const type = $(e).find('a').attr('href').match('.jpg') ? 'image' : 'video';
-         const url = $(e).find('a').attr('href');
-         return {
- type, 
-url
- };
-      }).get();
-
-      return {
-         status: data.length > 0,
-         data
-      };
-   } catch (error) {
-
-      return {
-         status: false,
-         msg: error.message
-      };
-   }
-} 
